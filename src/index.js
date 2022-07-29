84d212b7e79f3100773a52e3fd129446134af05c
evgeniydolgov <amyr214519@gmail.com>
2022-07-29 19:27:28 +0300

    init: create index

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word_morse = {};
+
+    for (key in MORSE_TABLE){
+        word_morse[MORSE_TABLE[key]] = key;
+    };
+    word_morse[' '] = ' ';
+    let answer = [];
+    let x = expr.split('');
+    console.log(x);
+    x.forEach(item => {
+        answer.push(word_morse[item])
+    })
+    console.log(answer);
+    let ibra = [];
+    answer.forEach(item => {
+        for (let i = 0; i < item.length; i++){
+            item[i] === '.'? ibra.push('10') : item[i] === '-'? ibra.push('11') : ibra.push('**********');
+        }
+        ibra.push('!')
+    });
+
+    let z = ibra.join('').split('!');
+    z.forEach(item => {
+        if(item === ''){
+        z.pop()
+        }
+    });
+    console.log(z)
+    let y = [];
+    z.forEach(item => {
+        let b = ('0000000000' + item)
+        y.push(b.substr(b.length -10));
+        
+    });
+    return z.join('')
 }
 
 module.exports = {
